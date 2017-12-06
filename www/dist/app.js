eb1b38eae97969a16b26c11936184cce85b8af56
Everton Dalla Corte <etodalla@gmail.com>
2017-12-06 21:15:25 -0200

    Finalizdo listagem de lanchoneste e de lanches iniciando pedido

--- www/dist/app.js
+++ www/dist/app.js
@@ -356,7 +356,40 @@ document.addEventListener("deviceready", onAppReady, false) ;
   function PedidoLocalController($state) {
     let vm = this;
     let slug = $state.params.slug;
-    vm.lanchonetes = [
+    vm.lanches = [
+		{
+			"id":1,
+			"nome":"Refrigerante Coca-cola 350ml",
+			"valor": "5.00",
+			"imagem":"coca.png"
+		},
+		{
+			"id":2,
+			"nome":"Hamburger de costela",
+			"valor": "20.00",
+			"imagem":"hamburger.png"
+		},
+		{
+			"id":3,
+			"nome":"Batata frita",
+			"valor": "10.00",
+			"imagem":"fritas.png"
+		},
+		{
+			"id":4,
+			"nome":"Tradicional caseiro",
+			"valor": "16.00",
+			"imagem":"tradicional_caseiro.jpg"
+		},
+		{
+			"id":5,
+			"nome":"Tradicional",
+			"valor": "11.00",
+			"imagem":"tradicional.jpg"
+		}
+	]
+
+	vm.lanchonetes = [
 		{
 			"nome":"koba",
 			"slug": "koba",
@@ -378,7 +411,6 @@ document.addEventListener("deviceready", onAppReady, false) ;
 			"imagem":"vegas.png"
 		}
 	]
-
 	vm.search = function(){
 		for (var i = 0; i < vm.lanchonetes.length; i++) {
 			if(vm.lanchonetes[i].slug == slug){
